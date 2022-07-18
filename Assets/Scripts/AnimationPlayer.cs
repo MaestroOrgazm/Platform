8e45bf3ab6b97378d57d55947d960b97cb6b4c7f
Maestro Orgasm <mikle.27051999@gmail.com>
2022-07-18 07:35:55 +0300

    4

--- Assets/Scripts/AnimationPlayer.cs
+++ Assets/Scripts/AnimationPlayer.cs
@@ -1,38 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 [RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(SpriteRenderer))]
-[RequireComponent(typeof(PlayerControl))]
 
 public class AnimationPlayer : MonoBehaviour
 {
     private SpriteRenderer _spriteRenderer;
     private Animator _animator;
     private string _speedAnimator = "Speed";
-    private PlayerControl _playerControl;
-    private float _movment;
 
     private void Start()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _animator = GetComponent<Animator>();
-        _playerControl = GetComponent<PlayerControl>();
     }
 
-    private void Update()
+    public void StartMoveAnimation(float movment)
     {
-        _movment = _playerControl.Movment;
-
-        if (_movment != 0)
-        {
-            _animator.SetFloat(_speedAnimator, 1);
-            _spriteRenderer.flipX = _movment < 0;
-        }
-        else
-        {
-            _animator.SetFloat(_speedAnimator, 0);
-        }
+        _animator.SetFloat(_speedAnimator, 1);
+        _spriteRenderer.flipX = movment < 0;
     }
 
+    public void StoptMoveAnimation()
+    {
+        _animator.SetFloat(_speedAnimator, 0);
+    }
 }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(AnimationPlayer))]
 
 
-public class PlayerControl : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
     public float Movment { get; private set; }
     private float _speed = 2;
     private float _jump = 9;
     private Rigidbody2D _rigidbody2D;
     private float _limit = 0.05f;
+    private AnimationPlayer _animationPlayer;
 
     private void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animationPlayer = GetComponent<AnimationPlayer>();
     }
 
     private void Update()
@@ -25,6 +29,15 @@ public class PlayerControl : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(_rigidbody2D.velocity.y) < _limit)
         {
             _rigidbody2D.AddForce(new Vector2(0, _jump), ForceMode2D.Impulse);
+        }
+
+        if (Movment != 0)
+        {
+            _animationPlayer.StartMoveAnimation(Movment);
+        }
+        else
+        {
+            _animationPlayer.StoptMoveAnimation();
         }
     }
 }
